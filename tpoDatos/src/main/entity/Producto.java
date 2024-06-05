e57d4d6ab7e224f06b844eb1aa802f88a85a9eaa
Bautista Salvalai <bautistasalvalai02@gmail.com>
2024-06-05 16:14:11 -0300

    Levanta el servidor

--- tpoDatos/src/main/entity/Producto.java
+++ tpoDatos/src/main/entity/Producto.java
@@ -1,19 +1,29 @@
 import org.springframework.data.annotation.Id;
+import org.springframework.data.cassandra.core.mapping.Column;
+import org.springframework.data.cassandra.core.mapping.Table;
 
 import jakarta.annotation.Generated;
 import lombok.Data;
 
 @Data
-@Entity
-//Hacer en Cassandra
+@Table("producto")
 public class Producto {
 
     @Id
     @GeneratedValue (strategy = GenerationType.IDENTITY)
     private long id;
 
+    @Column("nombre")
     private String nombre;
+
+    @Column("categoria")
     private String categoria;
+
+    @Column("precio")
     private double precio;
+
+    @Column("imagen")
     private String image;
+
+
 }
--- tpoDatos/src/main/repository/UserRepository.java
+++ tpoDatos/src/main/repository/UserRepository.java
@@ -3,7 +3,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, Integer>{
+public interface UserRepository extends MongoRepository<User,Long>{
 
     public User findByUsername(String username);
     public User findByEmail(String email);
